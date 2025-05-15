69de923af7355c9801bca7604cfe94a8c749fc60
kreshank <hansomecrank132@gmail.com>
2025-05-14 23:01:24 -0400

    Done experiences

--- js/exp_info.js
+++ js/exp_info.js
@@ -16,4 +16,38 @@ export const experiences = [
             'Cloud Computing',
         ]
     },
+    {
+        title: 'Cornell University<br>Teaching Assistant',
+        blurb: `August 2024-December 2024<br>CS 3780 - Introduction to Machine Learning Teaching Assistant.
+                Hosted office hours and assisted in developing theory and hands-on machine learning projects.
+                `,
+        tags: [
+            'Python',
+            'PyTorch',
+            'Machine Learning',
+        ]
+    },
+    {
+        title: 'CK Procleaners Inc.<br>Software Engineering Intern',
+        blurb: `June 2021-February 2022<br>Improve workplace efficiency by assisting in the design and 
+                deployment of an integrated database system to track and verify client orders, decreasing 
+                the number of lost garments by 10%. Produced and presented weekly reports analyzing 
+                client data trends using MySQL and Visual Basic.
+                `,
+        tags: [
+            'Visual Basic',
+            'MySQL',
+            'C++',
+            'Data Analytics',
+            'Python',
+        ]
+    },
+    {
+        title: 'Cornell Blockchain<br>Engineering Dev',
+        blurb: `January 2025-Current<br>Create Web3 and blockchain based projects, participate in hackathons.
+                `,
+        tags: [
+            'Web3',
+        ]
+    },
 ];
